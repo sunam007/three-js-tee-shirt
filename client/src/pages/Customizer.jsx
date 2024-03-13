4429c17403455cd079c168aa87fcbd2e9035b586
OSBORNE-JUNIOUS\sunam <asaduzzaman.sunam@gmail.com>
2024-03-13 23:56:32 +0600

    necessary imports on Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import {AnimatePresence,motion} from "framer-motion";
+import {useSnapshot} from "valtio"
+
+import state from '../store';
+import {download} from '../assets';
+import config from '../config/config';
+import {downloadCanvasToImage, reader} from '../config/helpers';
+import {EditorTabs,FilterTabs,DecalTypes} from '../config/constants';
+import {fadeAnimation, slideAnimation} from '../config/motion';
+
 
 const Customizer = () => {
     return (
